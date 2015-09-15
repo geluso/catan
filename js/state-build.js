905e3d12519192d856b6fb1451f9cf6939899755
Steve Geluso <stevegeluso@gmail.com>
2015-09-15 04:37:18 -0700

    restrictiong construction to actual cost after placement phase

--- js/state-build.js
+++ js/state-build.js
@@ -5,37 +5,37 @@ function StateBuild(board, thing) {
 
     // upgrade settlements to cities
     if (board.settlements[key]) {
-      // remove old settlement
-      delete board.settlements[key];
-
-      // replace it with the new city;
-      var city = new City(corner, "red");
-      board.cities[key] = city;
-
       if (!Resources.buyCity()) {
         $(".messages").text("Can't afford City.");
+      } else {
+        // remove old settlement
+        delete board.settlements[key];
+
+        // replace it with the new city;
+        var city = new City(corner, "red");
+        board.cities[key] = city;
       }
     } else {
       if (board.settlements[key] instanceof City) {
         return;
       }
 
-      var settlement = new Settlement(corner, "red");
-      board.settlements[key] = settlement;
-
       if (!Resources.buySettlement()) {
         $(".messages").text("Can't afford Settlement.");
+      } else {
+        var settlement = new Settlement(corner, "red");
+        board.settlements[key] = settlement;
       }
     }
   } else if (thing instanceof Edge) {
-    var edge = thing;
-    var key = edge.key();
-
-    var road = new Road(edge, "red");
-    board.roads[key] = road;
-
     if (!Resources.buyRoad()) {
       $(".messages").text("Can't afford Road.");
+    } else {
+      var edge = thing;
+      var key = edge.key();
+
+      var road = new Road(edge, "red");
+      board.roads[key] = road;
     }
   }
 }
