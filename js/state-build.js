4c5843427822c571f05dae047226a4f51c975aec
Steve Geluso <stevegeluso@gmail.com>
2015-09-18 14:01:21 -0700

    adding ghosting functions to build state

--- js/state-build.js
+++ js/state-build.js
@@ -39,3 +39,50 @@ function StateBuild(board, thing) {
     }
   }
 }
+
+StateBuild.shouldGhostRoad = function(board, edge) {
+  var result = false;
+
+  // the road can be placed if it's touching a settlement.
+  _.each(board.settlements, function(settlement) {
+    if (settlement.corner.equals(edge.c1) || settlement.corner.equals(edge.c2)) {
+      result = true;
+    }
+  });
+
+  _.each(board.cities, function(city) {
+    if (city.corner.equals(edge.c1) || city.corner.equals(edge.c2)) {
+      result = true;
+    }
+  });
+
+  // or if it's touching an existing road network.
+  _.each(board.roads, function(road) {
+    var neighbors = board.edgeGraph[road.edge.key()];
+    if (_.contains(neighbors, edge)) {
+      result = true;
+    }
+  });
+
+  return result;
+};
+
+StateBuild.shouldGhostCorner = function(board, corner) {
+  var result = true;
+
+  var takenCornerKeys = _.union(_.keys(board.settlements), _.keys(board.cities));
+
+  // can't place right next to other settements or citites.
+  _.each(takenCornerKeys, function(key) {
+    var neighbors = board.cornerGraph[key];
+    if (_.contains(neighbors, corner)) {
+      result = false;
+    }
+  });
+
+  return result;
+};
+
+StateBuild.shouldGhostRobber = function(board, tile) {
+  return true;
+};
