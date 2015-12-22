9d1783dea9c0dc348d5bb52c83ad2770ce1ebec8
Steve Geluso <stevegeluso@gmail.com>
2015-12-22 11:52:32 -0800

    renaming TILE_ARRANGEMENT

--- js/boards/four-player-board.js
+++ js/boards/four-player-board.js
@@ -27,7 +27,7 @@ FourPlayerBoard.prototype.init = function(tilespace) {
   var tokens = FourPlayerBoard.TOKEN_ARRANGEMENT.slice();
 
   // go through all our tiles and place the tokens and assign shuffled resources
-  _.each(FourPlayerBoard.TILE_ORDERED_ARRANGEMENT, function(position) {
+  _.each(FourPlayerBoard.TILE_ARRANGEMENT, function(position) {
     var tile = this.getTile(position.x, position.y);
     tile.resource = resources.pop(0);
     if (tile.resource !== DESERT) {
@@ -40,6 +40,6 @@ FourPlayerBoard.prototype.init = function(tilespace) {
 }
 
 FourPlayerBoard.TOKEN_ARRANGEMENT = [5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11];
-FourPlayerBoard.TILE_ORDERED_ARRANGEMENT = [{"x":525,"y":301},{"x":600,"y":258},{"x":675,"y":215},{"x":750,"y":258},{"x":825,"y":301},{"x":825,"y":387},{"x":825,"y":473},{"x":750,"y":516},{"x":675,"y":559},{"x":600,"y":516},{"x":525,"y":473},{"x":525,"y":387},{"x":600,"y":344},{"x":675,"y":301},{"x":750,"y":344},{"x":750,"y":430},{"x":675,"y":473},{"x":600,"y":430},{"x":675,"y":387}];
+FourPlayerBoard.TILE_ARRANGEMENT = [{"x":525,"y":301},{"x":600,"y":258},{"x":675,"y":215},{"x":750,"y":258},{"x":825,"y":301},{"x":825,"y":387},{"x":825,"y":473},{"x":750,"y":516},{"x":675,"y":559},{"x":600,"y":516},{"x":525,"y":473},{"x":525,"y":387},{"x":600,"y":344},{"x":675,"y":301},{"x":750,"y":344},{"x":750,"y":430},{"x":675,"y":473},{"x":600,"y":430},{"x":675,"y":387}];
 FourPlayerBoard.CENTER_TILE = {x: 675, y: 387};
 
