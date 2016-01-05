49ac3d3d5287e6f9dbeab553f4d5ffac93cff34c
Steve Geluso <stevegeluso@gmail.com>
2016-01-05 01:54:10 -0800

    adding util log function

--- js/log.js
+++ js/log.js
@@ -5,3 +5,23 @@ var LOG_TURNS = true;
 var LOG_EMPTY_TURNS = false;
 
 var LOG_ROBBER = true;
+
+var LOG = false;
+var LOG_INDENT = 0;
+
+function Log() {
+  var prefix = "";
+  if (LOG_INDENT > 0) {
+    for (var i = 0; i < LOG_INDENT; i++) {
+      prefix += "  ";
+    }
+  }
+
+  if (LOG) {
+    if (prefix) {
+      console.log(prefix, arguments);
+    } else {
+      console.log(arguments);
+    }
+  }
+}
